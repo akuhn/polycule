6a60676e8774e9936ea4e38ae97f95865f7acd68
Adrian Kuhn <akuhn@iam.unibe.ch>
2013-04-14 15:52:02 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polycule::Application.config.secret_token = 'ddd83e2ebb4864eb8b2f733ebc21a370343ad84bd5f0ae09e164679649da6bc2cd91033888d847014e51b3e1b3961161ada400cbd184ae71f3d36406249b4110'
