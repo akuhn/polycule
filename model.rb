e0ce58f2b29a208da5ac79e97f3fa7db10af2235
Adrian Kuhn <akuhn@iam.unibe.ch>
2013-05-13 03:00:04 -0700

    Connecting to MONGOHQ_URL in production.

--- model.rb
+++ model.rb
@@ -3,7 +3,14 @@ require 'mongo'
 require 'json'
 require 'net/http'
 
-DB = Mongo::Connection.new("localhost", 27017).db('polycule')
+if ENV['RACK_ENV'] == 'production'
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  db_name = db.path.gsub(/^\//, '')
+  DB = Mongo::Connection.new(db.host, db.port).db(db_name)
+  DB.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+else
+  DB = Mongo::Connection.new("localhost", 27017).db('polycule')
+end
 
 class Model < Hash
   class <<self
